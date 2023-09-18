bd98b74dc3e2c6332c38ef86ed1ddfc739ceac1e
Shivam Jadhav <127479563+shivam7695@users.noreply.github.com>
2023-09-18 23:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
-        document.body.innerText= "DOM load Success"
+        document.body.innerText= "DOM load success";
     });
     
